418b0ab5e549b63aed9cb504e87d5e8ed4a6aac3
Prince Singh <netprincesingh@gmail.com>
2026-05-01 17:48:01 +0530

    fix

--- tictactoe-server/config/db.js
+++ tictactoe-server/config/db.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+      throw new Error("MONGODB_URI is not defined in environment variables");
+    }
+    await mongoose.connect(uri);
     console.log("MongoDB Connected");
   } catch (err) {
-    console.error(err.message);
+    console.error("Database Connection Error:", err.message);
     process.exit(1);
   }
 };
